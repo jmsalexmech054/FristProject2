780a83745f185c640feee0167875864221fc80e3
jmsalexmech054 <jmsalexmech054@gmail.com>
2023-04-19 12:25:27 +0530

    add

---
+++ src/a1_firstProject/NewProgram.java
@@ -0,0 +1,12 @@
+package a1_firstProject;
+
+public class NewProgram {
+
+public static void main(String[] args) {
+		
+		for(int i=17;i<=24;i++){
+			System.out.println(i);
+		}
+		System.out.print("hello chenni");
+	}
+}
